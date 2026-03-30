b215d3453fa9d9291d47ee7fb1c55ddceca36167
Sade El Moudden <sade.elmoudden@edu.gbsl.ch>
2026-03-30 21:20:31 +0200

    Firebase connection

--- Resources/AppDelegate.swift
+++ Resources/AppDelegate.swift
@@ -4,4 +4,19 @@
 //
 //  Created by Sade El Moudden on 24.03.2026.
 //
+import UIKit
+import FirebaseCore
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
